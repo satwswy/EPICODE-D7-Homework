fdf61044efdc4d52b4066c93fc222d065cb4fbb3
marios <mgeorgak859@gmail.com>
2022-05-03 15:57:31 +0300

    updated exercise 1,2,3,4

---
+++ exercise.js
@@ -0,0 +1,34 @@
+/*
+Exercise 1
+A list of main datatypes is
+1.Numbers
+2.Strings
+3.Booleans
+4.Undefined
+5.Null
+
+Now let me try to explain some properties about each main datatype.Let's start with Numbers: 
+1.Numbers: as the name suggests is a numeric data type. We can give data a value of a number. Then this data becomes a number.
+2.Strings: A string data type is a text.This text can be anything we choose to write .
+3.Booleans: A boolean data type can only be either false or true. What it is depends on other variables.
+4.Undefined: When a datatype is Undefined that means that we need to say what this data is because it is nothing if we do not say that it is something.
+5.Null: When a data is null that means that the data is empty (it has no content). We can choose to put some content in it if we want to.
+*/
+
+/*
+Exercise 2
+I will try to explain what a variable is,
+A variable is a container for some content. We can select the initial content of the variable(container) when we first declare the variable.
+We can then change the content of the variable depending on what we want to do.
+*/
+
+//Exercise 3
+
+console.log (12+20)
+
+//Exercise 4
+
+let x
+x = 12
+console.log (x)
+
